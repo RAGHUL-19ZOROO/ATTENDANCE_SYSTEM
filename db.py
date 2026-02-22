bf7db305e3454531a4f3cd7bbd2f215b79327b38
RAGHUL-19ZOROO <raghul101120061@gmail.com>
2026-02-22 16:07:40 +0530

    hosting

--- db.py
+++ db.py
@@ -4,8 +4,9 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host=os.environ.get("MYSQLHOST"),
-        port=int(os.environ.get("MYSQLPORT")),   # 👈 VERY IMPORTANT
+        port=int(os.environ.get("MYSQLPORT")),
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE")
+        database=os.environ.get("MYSQLDATABASE"),
+        ssl_disabled=False   # ⭐ IMPORTANT for Railway
     )
