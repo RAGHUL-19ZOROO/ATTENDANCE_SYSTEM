2c3112cee99496704f6f524ac6190a10dadbf1b7
RAGHUL-19ZOROO <raghul101120061@gmail.com>
2026-02-22 17:23:10 +0530

    hi

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
-import os
 import mysql.connector
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get("MYSQLHOST"),
-        port=int(os.environ.get("MYSQLPORT")),
-        user=os.environ.get("MYSQLUSER"),
-        password=os.environ.get("MYSQLPASSWORD"),
-        database=os.environ.get("MYSQLDATABASE"),
-        ssl_disabled=False   # ⭐ IMPORTANT for Railway
+        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        port=4000,
+        user="3FXbxWqYyErQxC6.root",
+        password="izl3JviVOUVvTXuB",
+        database="attendance",
+        ssl_verify_cert=False,
+        ssl_verify_identity=False
     )
