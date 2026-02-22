ac83dc1da1bc09bdaea0c8b22f8133361219c90e
RAGHUL-19ZOROO <raghul101120061@gmail.com>
2026-02-22 15:48:39 +0530

    hosting

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host=os.environ.get("MYSQLHOST"),
-        port=int(os.environ.get("MYSQLPORT")),
+        port=int(os.environ.get("MYSQLPORT")),   # 👈 VERY IMPORTANT
         user=os.environ.get("MYSQLUSER"),
         password=os.environ.get("MYSQLPASSWORD"),
         database=os.environ.get("MYSQLDATABASE")
